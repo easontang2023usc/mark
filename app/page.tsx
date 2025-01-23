3390eb56c69822c5807c8fe8f7da0f1d67136b33
Eason <easontan@usc.edu>
2025-01-23 09:38:45 -0800

    initial deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
-      <img
+      <Image
         src="/mark_logo.png"
         alt="mark logo"
-        className="w-1/4 h-auto"
+        className="w-1/4 h-auto" // Optional Tailwind class for additional styling
+        width={0} // Set to 0 to use intrinsic width
+        height={0} // Set to 0 to use intrinsic height
+        sizes="25vw" // 25% of the viewport width
       />
     </div>
   );
