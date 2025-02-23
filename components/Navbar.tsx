efbf2ae9387d9bba23da931ab2add28ea9c61eff
Eason <easontan@usc.edu>
2025-02-23 14:44:00 -0800

    added waitlist signup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,29 @@
-"use client";
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
-import "../styles/Navbar.css";
-import Typography from "@/components/ui/Typography";
+import WaitlistDialog from "./waitlistForm";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      {/* Logo */}
-      <div className="logo">
-        <img src="/Mark_Assets/Mark_logo_only_black.png" alt="Mark Logo" className="logo-img" />
-      </div>
+  const [isOpen, setIsOpen] = useState(false);
 
-      {/* Navigation Links */}
-      <div className="nav-links">
-        <Link href="#manifesto">
-          <Typography variant="body3">Manifesto</Typography>
-        </Link>
-        <Link href="#waitlist" className="joinwaitlist">
-          <Typography variant="body3" className="text-white">Join Waitlist</Typography>
-        </Link>
-      </div>
-    </nav>
+  return (
+    <>
+      <nav className="fixed top-[2%] left-1/2 transform -translate-x-1/2 w-[90%] max-w-[900px] rounded-full bg-[rgba(84,84,84,0.6)] backdrop-blur-md shadow-md flex items-center justify-between px-6 py-3 z-50">
+        {/* Logo */}
+        <div>
+          <Link href="/">
+            <img
+              src="/Mark_Assets/Mark_logo_full_white.png"
+              alt="Mark Logo"
+              className="h-7 w-auto"
+            />
+          </Link>
+        </div>
+
+        <WaitlistDialog />
+      
+      </nav>
+    </>
   );
 }
